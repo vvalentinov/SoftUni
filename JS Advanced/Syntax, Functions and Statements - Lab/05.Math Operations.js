6eb4351201cbcff253c6c975a7654bca20338136
Valentin <valentinvalentinov027@gmail.com>
2022-05-19 13:15:50 +0300

    Added Math Operations Solution

---
+++ JS Advanced/Syntax, Functions and Statements - Lab/05.Math Operations.js
@@ -0,0 +1,25 @@
+function solve(firstNumber, secondNumber, operator) {
+    switch (operator) {
+        case '+':
+            console.log(firstNumber + secondNumber);
+            break;
+        case '-':
+            console.log(firstNumber - secondNumber);
+            break;
+        case '*':
+            console.log(firstNumber * secondNumber);
+            break;
+        case '/':
+            console.log(firstNumber / secondNumber);
+            break;
+        case '%':
+            console.log(firstNumber % secondNumber);
+            break;
+        case '**':
+            console.log(firstNumber ** secondNumber);
+            break;
+    }
+}
+
+solve(5, 6, '+');
+solve(3, 5.5, '*');
