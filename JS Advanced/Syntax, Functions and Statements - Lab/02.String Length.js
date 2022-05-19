c8ec128d8cff328536cce1c719ca55ba1dc120f5
Valentin <valentinvalentinov027@gmail.com>
2022-05-19 13:00:41 +0300

    Added String Length problem

---
+++ JS Advanced/Syntax, Functions and Statements - Lab/02.String Length.js
@@ -0,0 +1,7 @@
+function stringLength(first, second, third) {
+    const sum = first.length + second.length + third.length;
+    console.log(sum);
+    console.log(Math.floor(sum / 3));
+}
+
+stringLength('pasta', '5', '22.3');
