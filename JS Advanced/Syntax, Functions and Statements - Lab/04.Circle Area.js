1c3feba080441cb2c6a041da74349185980eb6a0
Valentin <valentinvalentinov027@gmail.com>
2022-05-19 13:11:10 +0300

    Added Circle Area Solution

---
+++ JS Advanced/Syntax, Functions and Statements - Lab/04.Circle Area.js
@@ -0,0 +1,10 @@
+function area(input) {
+    if (typeof input == 'number') {
+        console.log((input ** 2 * Math.PI).toFixed(2));
+    } else {
+        console.log(`We can not calculate the circle area, because we receive a ${typeof input}.`);
+    }
+}
+
+area(5);
+area('name');
