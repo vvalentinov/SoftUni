c989e9bc83aed29baf257a072f84f33c6ef2d340
Valentin <valentinvalentinov027@gmail.com>
2022-05-19 15:56:59 +0300

    Added Time to Walk Solution

---
+++ JS Advanced/Syntax, Functions and Statements - Exercise/05.Time to Walk.js
@@ -0,0 +1,15 @@
+function timeToWalk(steps, footprint, studentSpeed) {
+    let distance = steps * footprint;
+    let speed = studentSpeed * 1000 / 3600;
+    let rest = Math.floor(distance / 500) * 60;
+    let time = (distance / speed) + rest;
+
+    let hours = Math.floor(time / 3600).toFixed(0).padStart(2, '0');
+    let minutes = Math.floor(time / 60).toFixed(0).padStart(2, '0');
+    let seconds = (time % 60).toFixed(0).padStart(2, '0');
+
+    console.log(`${hours}:${minutes}:${seconds}`);
+}
+
+timeToWalk(4000, 0.60, 5);
+timeToWalk(2564, 0.70, 5.5);
