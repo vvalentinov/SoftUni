845e1c34060eb3c142757d325d4e381b0599cb33
Valentin <valentinvalentinov027@gmail.com>
2022-05-20 13:12:36 +0300

    Added Equal Neighbors Solution

---
+++ JS Advanced/Arrays and Nested Arrays - Lab/11.Equal Neighbors.js
@@ -0,0 +1,25 @@
+function solve(inputMatrix) {
+    let numberOfPairs = 0;
+    inputMatrix.forEach((array, index) => {
+        array.forEach((el, idx) => {
+            if (idx + 1 < array.length && array[idx] === array[idx + 1]) { numberOfPairs++; }
+            if (index + 1 < inputMatrix.length) {
+                if (inputMatrix[index + 1][idx] === el) { numberOfPairs++; }
+            }
+        });
+    });
+    console.log(numberOfPairs);
+}
+
+solve(
+    [['2', '3', '4', '7', '0'],
+    ['4', '0', '5', '3', '4'],
+    ['2', '3', '5', '4', '2'],
+    ['9', '8', '7', '5', '4']]
+);
+
+solve(
+    [['test', 'yes', 'yo', 'ho'],
+    ['well', 'done', 'yo', '6'],
+    ['not', 'done', 'yet', '5']]
+);
