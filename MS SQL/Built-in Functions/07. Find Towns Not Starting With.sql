db7714c66fc54d4ef6bc314d06a9e03b855465d2
Valentin <valentinvalentinov027@gmail.com>
2022-06-22 20:00:13 +0300

    Add MS SQL - Built-in Functions Problems Solutions

---
+++ MS SQL/Built-in Functions/07. Find Towns Not Starting With.sql
@@ -0,0 +1,3 @@
+SELECT * FROM Towns
+WHERE [NAME] LIKE '[^RBD]%'
+ORDER BY [Name]
---
+++ MS SQL/Built-in Functions/16. Get Users with IPAddress Like Pattern.sql
@@ -0,0 +1,4 @@
+SELECT Username, IpAddress AS [IP Address]
+FROM Users
+WHERE IpAddress LIKE '___.1_%._%.___' 
+ORDER BY Username
