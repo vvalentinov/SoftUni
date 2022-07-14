ab223a0eb6a3f9eec689ee2789d46ee3a1875681
Valentin <valentinvalentinov027@gmail.com>
2022-07-14 17:25:07 +0300

    Add Entity Framework Core - Create BookShop Project and add Advanced Querying Solution

---
+++ Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace BookShop
+{
+    using BookShop.Data;
+    using BookShop.Initializer;
+    using BookShop.Models.Enums;
+    using System.Text;
+
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string command = Console.ReadLine();
+
+            Console.WriteLine(GetBooksByAgeRestriction(db, command));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var ageRestriction = Enum.Parse<AgeRestriction>(command, true);
+
+            var titles = context.Books
+                                .Where(x => x.AgeRestriction == ageRestriction)
+                                .Select(x => x.Title)
+                                .OrderBy(x => x)
+                                .ToList();
+
+            StringBuilder builder = new StringBuilder();
+
+            foreach (var title in titles)
+            {
+                builder.AppendLine(title);
+            }
+
+            return builder.ToString().TrimEnd();
+        }
+    }
+}
