c6d595c943b42a6e4bdc7b8b84f59dada7b266ad
Valentin <valentinvalentinov027@gmail.com>
2023-09-14 11:58:39 +0300

    Handle request for home page

--- src/JS Back-End/Cat Shelter/index.js
+++ src/JS Back-End/Cat Shelter/index.js
@@ -1,10 +1,30 @@
 const http = require('http');
+const fs = require('fs/promises');
+const path = require('path');
 
 const port = 5001;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     if (req.url == '/') {
-        res.write('OK');
+        const homeHtmlPath = path.resolve(__dirname, './views/home/index.html');
+        const homeHtml = await fs.readFile(homeHtmlPath);
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(homeHtml);
+        res.end();
+    } else if (req.url.endsWith('site.css')) {
+        const siteCssPath = path.resolve(__dirname, './content/styles/site.css');
+        const siteCss = await fs.readFile(siteCssPath);
+
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.write(siteCss);
+        res.end();
+    } else if (req.url.endsWith('pawprint.ico')) {
+        const pawprintIcoPath = path.resolve(__dirname, './content/images/pawprint.ico');
+        const pawprintIco = await fs.readFile(pawprintIcoPath);
+
+        res.writeHead(200, { 'Content-Type': 'image/png' });
+        res.write(pawprintIco);
         res.end();
     }
 });
