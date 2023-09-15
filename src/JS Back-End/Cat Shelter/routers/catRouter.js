a60927b447aff8c39008a5bb81d83560c6be5d7f
Valentin <valentinvalentinov027@gmail.com>
2023-09-15 19:37:44 +0300

    Add cat breed to breeds.json

--- src/JS Back-End/Cat Shelter/routers/catRouter.js
+++ src/JS Back-End/Cat Shelter/routers/catRouter.js
@@ -10,11 +10,23 @@ const catRouter = async (req, res) => {
         res.write(addBreedHtml);
         res.end();
     } else if (req.url == '/cats/add-breed' && req.method === 'POST') {
-        req.on('data', (data) => {
-            let input = data.toString().split('=')[1];
-            let breed = input.replace('+', ' ');
+        req.on('data', async (data) => {
+            const input = data.toString().split('=')[1];
+            const breed = input.replaceAll('+', ' ');
+
+            const breedsDbPath = path.resolve(__dirname, '../data/breeds.json');
+            const breedsDb = await fs.readFile(breedsDbPath);
+
+            if (breedsDb.length) {
+                let breeds = Object.values(JSON.parse(breedsDb));
+                breeds.push(breed);
+                await fs.writeFile(breedsDbPath, JSON.stringify(breeds, null, 4));
+            } else {
+                await fs.writeFile(breedsDbPath, JSON.stringify([breed], null, 4));
+            }
         });
 
+        res.writeHead(301, { Location: '/' });
         res.end();
     } else if (req.url == '/cats/add-cat') {
         const addCatHtmlPath = path.resolve(__dirname, '../views/addCat.html');
