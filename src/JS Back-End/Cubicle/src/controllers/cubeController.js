51e3f749f5f5b9b71efe1168daba43f71273995e
Valentin <valentinvalentinov027@gmail.com>
2023-09-22 15:41:45 +0300

    Add action for post request when creating cube

--- src/JS Back-End/Cubicle/src/controllers/cubeController.js
+++ src/JS Back-End/Cubicle/src/controllers/cubeController.js
@@ -4,4 +4,15 @@ router.get('/create', (req, res) => {
     res.render('cubes/create');
 });
 
+router.post('/create', (req, res) => {
+    const {
+        name,
+        description,
+        imageUrl,
+        difficultyLevel
+    } = req.body;
+
+    res.send('Creating cube...');
+});
+
 module.exports = router;
