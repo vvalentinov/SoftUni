93e184ae58bf547f9df7d2516ea1463229f4de51
Valentin <valentinvalentinov027@gmail.com>
2023-10-04 19:03:50 +0300

    Implement user logout functionality

--- src/JS Back-End/Exams/13 Apr 2023/Gaming Team/src/controllers/userController.js
+++ src/JS Back-End/Exams/13 Apr 2023/Gaming Team/src/controllers/userController.js
@@ -38,4 +38,9 @@ router.post('/login', async (req, res) => {
     }
 });
 
+router.get('/logout', (req, res) => {
+    res.clearCookie(JWT_KEY);
+    res.redirect('/');
+})
+
 module.exports = router;
