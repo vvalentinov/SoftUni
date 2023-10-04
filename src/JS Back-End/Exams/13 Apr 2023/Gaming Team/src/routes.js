523eed0cbef0af63c4ff849b67bcc230997771b0
Valentin <valentinvalentinov027@gmail.com>
2023-10-04 18:33:38 +0300

    Add user controller and show register page

--- src/JS Back-End/Exams/13 Apr 2023/Gaming Team/src/routes.js
+++ src/JS Back-End/Exams/13 Apr 2023/Gaming Team/src/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
+const userController = require('./controllers/userController');
 const homeController = require('./controllers/homeController');
 
+router.use('/user', userController);
 router.use(homeController);
 
 module.exports = router;
