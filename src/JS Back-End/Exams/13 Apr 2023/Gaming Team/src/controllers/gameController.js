5b4e9be46983694b2b9ca60284bf3ca1ac1919d7
Valentin <valentinvalentinov027@gmail.com>
2023-10-04 19:19:23 +0300

    Implement create game offer functionality

--- src/JS Back-End/Exams/13 Apr 2023/Gaming Team/src/controllers/gameController.js
+++ src/JS Back-End/Exams/13 Apr 2023/Gaming Team/src/controllers/gameController.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 
+const { getErrorMessage } = require('../utils/errorHelper');
+
 const gameService = require('../services/gamesService');
+
+const { isAuthenticated } = require('../middlewares/authMiddleware');
 
 router.get('/all', (req, res) => {
     res.render('games/catalog');
@@ -10,5 +14,15 @@ router.get('/create', (req, res) => {
     res.render('games/create');
 });
 
+router.post('/create', isAuthenticated, async (req, res) => {
+    const gameData = req.body;
+
+    try {
+        await gameService.create({ ...gameData, owner: req.user._id });
+        res.redirect('/games/all');
+    } catch (err) {
+        res.render('games/create', { error: getErrorMessage(err) });
+    }
+});
 
 module.exports = router;
