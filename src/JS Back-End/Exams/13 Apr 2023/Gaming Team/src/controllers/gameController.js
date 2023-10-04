cb56915a6f74120caa3d0f0de748ecdc0ddedba8
Valentin <valentinvalentinov027@gmail.com>
2023-10-04 19:59:02 +0300

    Implement buy game functionality

--- src/JS Back-End/Exams/13 Apr 2023/Gaming Team/src/controllers/gameController.js
+++ src/JS Back-End/Exams/13 Apr 2023/Gaming Team/src/controllers/gameController.js
@@ -26,4 +26,16 @@ router.post('/create', isAuthenticated, async (req, res) => {
     }
 });
 
+router.get('/details/:gameId', async (req, res) => {
+    const game = await gameService.getGameWithId(req.params.gameId).lean();
+    const isOwner = req.user?._id == game.owner;
+    const hasBoughtTheGame = game.boughtBy.some(x => x.toString() == req.user?._id);
+    res.render('games/details', { game, isOwner, hasBoughtTheGame });
+});
+
+router.get('/buy/:gameId', async (req, res) => {
+    await gameService.buyGame(req.params.gameId, req.user._id);
+    res.redirect(`/games/details/${req.params.gameId}`);
+});
+
 module.exports = router;
--- src/JS Back-End/Exams/13 Apr 2023/Gaming Team/src/services/gamesService.js
+++ src/JS Back-End/Exams/13 Apr 2023/Gaming Team/src/services/gamesService.js
@@ -3,3 +3,11 @@ const Game = require('../models/Game');
 exports.create = (gameData) => Game.create(gameData);
 
 exports.getAll = () => Game.find();
+
+exports.getGameWithId = (gameId) => Game.findById(gameId);
+
+exports.buyGame = async (gameId, userId) => {
+    const game = await Game.findById(gameId);
+    game.boughtBy.push(userId);
+    return game.save();
+};
