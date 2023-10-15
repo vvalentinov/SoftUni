bfed2db3287c1397a3944bd3ebc429ff0705df0a
Valentin <valentinvalentinov027@gmail.com>
2023-10-15 18:31:39 +0300

    Implement wish book functionality

--- src/JS Back-End/Exams/18 Aug 2022/Book Talk/src/controllers/bookController.js
+++ src/JS Back-End/Exams/18 Aug 2022/Book Talk/src/controllers/bookController.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const bookService = require('../services/bookService');
 const { getErrorMessage } = require('../utils/errorHelper');
+const { isAuthenticated } = require('../middlewares/authMiddleware');
 
 router.get('/all', async (req, res) => {
     const books = await bookService.getAllBooks().lean();
@@ -12,14 +13,30 @@ router.get('/create', (req, res) => {
     res.render('books/create');
 });
 
-router.post('/create', async (req, res) => {
+router.post('/create', isAuthenticated, async (req, res) => {
     const bookReviewData = req.body;
+    const owner = req.user._id;
     try {
-        await bookService.createBookReview(bookReviewData);
+        await bookService.createBookReview(bookReviewData, owner);
         res.redirect('/book/all');
     } catch (error) {
         res.render('books/create', { errorMessage: getErrorMessage(error) });
     }
+});
+
+router.get('/details/:bookId', async (req, res) => {
+    const bookId = req.params.bookId;
+    const book = await bookService.getBookById(bookId).lean();
+    const isOwner = req.user?._id == book.owner;
+    const hasWishedBook = book.wishingList.some(x => x.toString() == req.user?._id);
+    res.render('books/details', { book, isOwner, hasWishedBook });
+});
+
+router.get('/wish/:bookId', isAuthenticated, async (req, res) => {
+    const bookId = req.params.bookId;
+    const userId = req.user._id;
+    await bookService.wishBook(bookId, userId);
+    res.redirect(`/book/details/${bookId}`);
 });
 
 module.exports = router;
--- src/JS Back-End/Exams/18 Aug 2022/Book Talk/src/services/bookService.js
+++ src/JS Back-End/Exams/18 Aug 2022/Book Talk/src/services/bookService.js
@@ -2,4 +2,12 @@ const Book = require('../models/Book');
 
 exports.getAllBooks = () => Book.find({});
 
-exports.createBookReview = (bookReviewData) => Book.create({ ...bookReviewData });
+exports.createBookReview = (bookReviewData, owner) => Book.create({ ...bookReviewData, owner });
+
+exports.getBookById = (bookId) => Book.findById(bookId);
+
+exports.wishBook = async (bookId, userId) => {
+    const book = await Book.findById(bookId);
+    book.wishingList.push(userId);
+    await book.save();
+};
