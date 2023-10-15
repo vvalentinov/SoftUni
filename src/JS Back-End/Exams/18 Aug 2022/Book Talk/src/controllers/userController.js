51f5d4e5deb82d9f4b15866442a5c54b7514a4a0
Valentin <valentinvalentinov027@gmail.com>
2023-10-15 17:47:31 +0300

    Implement login functionality

--- src/JS Back-End/Exams/18 Aug 2022/Book Talk/src/controllers/userController.js
+++ src/JS Back-End/Exams/18 Aug 2022/Book Talk/src/controllers/userController.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const userService = require('../services/usersService');
 const { JWT_KEY } = require('../constants/constants');
-const { getErrorMessage } = require('../utils/errorHelper')
+const { getErrorMessage } = require('../utils/errorHelper');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -16,6 +16,21 @@ router.post('/register', async (req, res) => {
         res.redirect('/');
     } catch (error) {
         res.render('users/register', { errorMessage: getErrorMessage(error) });
+    }
+});
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const token = await userService.login(email, password);
+        res.cookie(JWT_KEY, token);
+        res.redirect('/');
+    } catch (error) {
+        res.render('users/login', { errorMessage: getErrorMessage(error) });
     }
 });
 
