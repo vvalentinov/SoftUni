3497b5eab47b11d35adf0d49f6c754d899389fec
Valentin <valentinvalentinov027@gmail.com>
2023-10-18 16:46:21 +0300

    Show create offer page

--- src/JS Back-End/Exams/25 Jun 2022/Crypto Trade/src/routes.js
+++ src/JS Back-End/Exams/25 Jun 2022/Crypto Trade/src/routes.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const userController = require('./controllers/userController');
+const cryptoController = require('./controllers/cryptoController');
 
 router.use(homeController);
 router.use('/user', userController);
+router.use('/crypto', cryptoController);
 
 module.exports = router;
