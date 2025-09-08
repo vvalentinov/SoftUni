8af1dca179e38a62754160a060fa03ff129c9c1e
Valentin <valentinvalentinov027@gmail.com>
2025-09-08 15:16:25 +0300

    Add Basketball Equipment problem solution in C++

---
+++ src/Programming Basics C++/02. FirstStepsInCodingPractice/08. BasketballEquipment/08. BasketballEquipment.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int annualTax;
+
+	cin >> annualTax;
+
+	double basketballShoes = annualTax - (annualTax * 0.4);
+	double basketballUniform = basketballShoes - (basketballShoes * 0.2);
+	double basketball = basketballUniform / 4;
+	double basketballAccessories = basketball / 5;
+
+	double result =
+		basketballShoes +
+		basketballUniform +
+		basketball +
+		basketballAccessories +
+		annualTax;
+
+	cout << result << endl;
+
+	return 0;
+}
